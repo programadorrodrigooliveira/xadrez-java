9e91e002fc66513cb86971fdf3e93118c2f4ec73
programadorrodrigooliveira <47319818+programadorrodrigooliveira@users.noreply.github.com>
2020-04-08 23:25:12 -0300

    Iniciando a implementação de Board e Piece

---
+++ src/boardgame/Board.java
@@ -0,0 +1,15 @@
+package boardgame;
+
+public class Board {
+	private int rows;
+	private int columns;
+	private Piece[][] pieces;
+	
+	public Board(int rows, int columns) {
+		super();
+		this.rows = rows;
+		this.columns = columns;
+		this.pieces = Piece[rows][columns];
+	}
+
+}
